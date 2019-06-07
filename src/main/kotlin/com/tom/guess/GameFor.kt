723ee9a726e378a910e5595008f0425f287f5724
Adolph9527 <log9527@gmail.com>
2019-06-07 11:38:14 +0800

    latest

---
+++ src/main/kotlin/com/tom/guess/GameFor.kt
@@ -0,0 +1,22 @@
+package com.tom.guess
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val secret = Random().nextInt(10)+1
+    println(secret)
+    val scanner = Scanner(System.`in`)
+    for(i in 1..4) {
+        print("Please insert a number (${i}/4): ")
+        var num = scanner.nextInt()
+        println("第${i}次   $num")
+        if (num >secret){
+            println("lower")
+        }else if (num < secret ){
+            println("hight")
+        }else{
+            println("bingo!! the secret number is $num")
+            break;
+        }
+    }
+}
